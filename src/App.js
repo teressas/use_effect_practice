5afa6f63116d6a84d25f297151706033a624a9b8
Teressa Sung <teressas.hawaii@gmail.com>
2022-02-05 20:23:10 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React, { useState, useEffect } from 'react';
+import UseEffectExample from './components/UseEffectExample';
+
+function App() {
+  return (
+    <div className="App">
+      <UseEffectExample />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/UseEffectExample.jsx
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react';
+
+const Example = (props) => {
+    const [people, setPeople] = useState([]);
+ 
+    useEffect(() => {
+        fetch('https://swapi.dev/api/people/')
+            .then(response => response.json())
+            .then(response => setPeople(response.results))
+    }, []);
+ 
+    return (
+        <div>
+            {people.length > 0 && people.map((person, index)=>{
+                return (<div key={index}>{person.name}</div>)
+            })}
+        </div>
+    );
+}
+export default Example;
+
